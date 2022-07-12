30127227b6bd215dad2a3c3feaa6653a59b1aa7f
Jonathan Davies <jpds@protonmail.com>
2022-07-12 20:27:13 +0100

    unikernel.ml: Use Tcpip instead of Mirage_stack.

--- unikernel.ml
+++ unikernel.ml
@@ -1,5 +1,5 @@
 (* (c) 2017, 2018 Hannes Mehnert, all rights reserved *)
-module Main (R : Mirage_random.S) (P : Mirage_clock.PCLOCK) (M : Mirage_clock.MCLOCK) (T : Mirage_time.S) (S : Mirage_stack.V4V6) = struct
+module Main (R : Mirage_random.S) (P : Mirage_clock.PCLOCK) (M : Mirage_clock.MCLOCK) (T : Mirage_time.S) (S : Tcpip.Stack.V4V6) = struct
   module D = Dns_resolver_mirage.Make(R)(P)(M)(T)(S)
 
   let start _r _pclock _mclock _ s =
