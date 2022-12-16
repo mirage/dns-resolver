14a5570b82964d0398381cdf6b15f2252de34d6e
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-12-16 14:29:09 +0100

    Delete pin and disallow the support of DNSSEC

--- config.ml
+++ config.ml
@@ -5,11 +5,12 @@ open Mirage
 let dns_handler =
   let packages =
     [
-      package "logs" ;
-      package "dns" ~max:"6.1.0";
-      package "dns-server" ~max:"6.1.0";
-      package "dns-mirage" ~max:"6.1.0";
-      package ~sublibs:[ "mirage" ] "dns-resolver";
+      package "logs";
+      package "dns" ~min:"6.4.1" ~max:"6.5.0";
+      package "dns-server" ~min:"6.4.1" ~max:"6.5.0";
+      package "dns-mirage" ~min:"6.4.1" ~max:"6.5.0";
+      package "dnssec" ~min:"6.4.1" ~max:"6.5.0";
+      package ~sublibs:[ "mirage" ] "dns-resolver" ~min:"6.4.1" ~max:"6.5.0";
     ]
   in
   foreign
